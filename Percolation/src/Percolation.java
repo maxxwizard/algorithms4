7fb55cdc611b23c38e2964adb8641424fdc0b0e3
maxxwizard <matthewhuynh88@gmail.com>
2016-09-06 23:58:41 -0400

    Percolation module

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,35 @@
+/**
+ * Created by Matthew Huynh on 9/6/2016.
+ */
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    public Percolation(int n) {
+        throw new UnsupportedOperationException();
+        // create n-by-n grid, with all sites blocked
+    }
+    public void open(int i, int j) {
+        throw new UnsupportedOperationException();
+        // open site (row i, column j) if it is not open already
+    }
+    public boolean isOpen(int i, int j) {
+        throw new UnsupportedOperationException();
+        // is site (row i, column j) open?
+    }
+    public boolean isFull(int i, int j) {
+        throw new UnsupportedOperationException();
+        // is site (row i, column j) full?
+    }
+    public boolean percolates() {
+        throw new UnsupportedOperationException();
+        // does the system percolate?
+    }
+
+    public static void main(String[] args) {
+        // test client (optional)
+        System.out.println("my first PA");
+    }
+}
