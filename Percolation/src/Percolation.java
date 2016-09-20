1fec9c11ed6a70a8a059843be64047196c664d62
maxxwizard <matthewhuynh88@gmail.com>
2016-09-19 22:31:44 -0400

    Percolation module

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -2,34 +2,214 @@
  * Created by Matthew Huynh on 9/6/2016.
  */
 
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+
+    WeightedQuickUnionUF map;
+    int gridSize; // this is necessary to navigate our array
+    int[] openFull; // -1 = closed, 0 = open, 1 = full
+
+    // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
-        throw new UnsupportedOperationException();
-        // create n-by-n grid, with all sites blocked
+        // validate param
+        if (n <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        map = new WeightedQuickUnionUF(n*n);
+        gridSize = n;
+        // each site should be initialized as closed
+        openFull = new int[n * n];
+        for (int i = 0; i < openFull.length; i++) {
+            openFull[i] = -1;
+        }
+
     }
-    public void open(int i, int j) {
-        throw new UnsupportedOperationException();
-        // open site (row i, column j) if it is not open already
+
+    // open site (row i, column j) if it is not open already
+    public void open(int row, int col) {
+        System.out.println("opening (" + row + ", " + col + ")");
+        // the upper-left site is indexed (1, 1) but it will be presented as (0, 0) in our map
+        validatePositionOneBased(row, col);
+
+        // mark the site as open
+        int newSiteArrIdx = getArrayIndex(row, col);
+        openFull[newSiteArrIdx] = 0;
+
+        // check if any adjacent sites are open and union with them if so
+        unionNeighbors(row, col);
+
+        // if the newly opened site is a top node, promote the site from open to full
+        if (row == 1) {
+            openFull[newSiteArrIdx] = 1;
+        }
+
     }
+
+    // tries to union a neighboring site if site node exists
+    private void tryUnionNeighbor(int newSiteRow, int newSiteCol, int neighborRow, int neighborCol) {
+        try {
+            if (isOpen(neighborRow, neighborCol)) {
+                int newSiteIdx = getArrayIndex(newSiteRow, newSiteCol);
+                int neighborIdx = getArrayIndex(neighborRow, neighborCol);
+                System.out.println("tryUnionNeighbor: newSiteIdx=" + newSiteIdx + ", neighborIdx=" + neighborIdx);
+                map.union(newSiteIdx, neighborIdx);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            // do nothing
+        }
+    }
+
+    // find the above, below, left, and right of the new site and union if possible
+    private void unionNeighbors(int row, int col) {
+        // above
+        tryUnionNeighbor(row, col, row-1, col);
+
+        // below
+        tryUnionNeighbor(row, col, row+1, col);
+
+        // left
+        tryUnionNeighbor(row, col, row, col-1);
+
+        // right
+        tryUnionNeighbor(row, col, row, col+1);
+    }
+
+    // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
-        throw new UnsupportedOperationException();
-        // is site (row i, column j) open?
+        // the upper-left site is indexed (1, 1) but it will be presented as (0, 0) in our map
+        validatePositionZeroBased(i-1, j-1);
+
+        int pos = (i-1)*gridSize + (j-1);
+        boolean result = openFull[pos] == 0;
+        return result;
     }
+
+    // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
-        throw new UnsupportedOperationException();
-        // is site (row i, column j) full?
+        // the upper-left site is indexed (1, 1) but it will be presented as (0, 0) in our map
+        validatePositionZeroBased(i-1, j-1);
+
+        int pos = (i-1)*gridSize + (j-1);
+        boolean result = openFull[pos] == 1;
+        return result;
     }
+
+    // checks that indices i and j are inside the map bounds
+    private void validatePositionZeroBased(int i, int j) {
+        if (i < 0 || i >= gridSize || j < 0 || j >= gridSize) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+    }
+
+    // checks that indices i and j are inside the map bounds
+    private void validatePositionOneBased(int i, int j) {
+        if (i < 1 || i > gridSize || j < 1 || j > gridSize) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+    }
+
+    private int getArrayIndex(int row, int col) {
+        return (row-1)*gridSize + (col-1);
+    }
+
+    // does the system percolate?
     public boolean percolates() {
-        throw new UnsupportedOperationException();
-        // does the system percolate?
+        System.out.println("percolating system... ");
+        // for each open site that is connected to a top node, promote the site from open to full (skip top row)
+        for (int row = 2; row <= gridSize; row++) {
+            for (int col = 1; col <= gridSize; col++) {
+                int currentSiteIdx = getArrayIndex(row, col);
+                //System.out.print("Checking if (" + row + ", " + col + ") is connected to a top-row full site... ");
+                if (hasConnectionToOpenTopRowSite(row, col)) {
+                    //System.out.println("yes.");
+                    openFull[currentSiteIdx] = 1;
+                } else {
+                    //System.out.println("no.");
+                }
+            }
+        }
+
+        // check if there is a component that has a connection between a site in the top and a site in the bottom
+        for (int topSite = 0; topSite < gridSize; topSite++) {
+            for (int j = 0; j < gridSize; j++) {
+                int bottomSite = (gridSize-1)*gridSize + j;
+                //System.out.println("checking if topSite " + topSite + " is connected with bottomSite " + bottomSite + "...");
+                if (map.connected(topSite, bottomSite)) {
+                    //System.out.println(" true");
+                    return true;
+                } else {
+                    //System.out.println(" false");
+                }
+            }
+        }
+
+        return false; // there is no connection thus the system must not percolate
+    }
+
+    private boolean hasConnectionToOpenTopRowSite(int row, int col) {
+        validatePositionOneBased(row, col);
+
+        int newSiteIdx = getArrayIndex(row, col);
+        // iterate through top row
+        for (int currentCol = 1; currentCol <= gridSize; currentCol++) {
+            // if current column's top-most site is full
+            if (isFull(1, currentCol)) {
+                // check if the target site is connected to this full site
+                //System.out.println("site (1, " + currentCol + ") is full");
+                int currentColArrayIndex = getArrayIndex(1, currentCol);
+                //System.out.println("newSiteIdx " + newSiteIdx + " is connected to currentColArrayIndex " + currentColArrayIndex + "?");
+                if (map.connected(newSiteIdx, currentColArrayIndex)) {
+                    //System.out.println("yes");
+                    return true;
+                } else {
+                    //System.out.println("no");
+                }
+            }
+
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         // test client (optional)
         System.out.println("my first PA");
+
+        // initialize a map
+        Percolation p = new Percolation(3);
+
+        p.printDebugGrid();
+        assert p.isOpen(3, 3) == false;
+        assert p.isFull(2, 2) == false;
+        p.open(3, 2);
+        p.open(2, 2);
+        p.printDebugGrid();
+        assert p.isFull(2, 2) == false;
+        assert p.isOpen(3, 2) == true;
+        assert p.isOpen(2, 2) == true;
+        boolean perc = p.percolates();
+        p.printDebugGrid();
+        assert perc == false;
+
+        p.open(1, 2);
+        p.printDebugGrid();
+        perc = p.percolates();
+        p.printDebugGrid();
+        assert p.isFull(1, 2);
+        assert p.isFull(2, 2);
+        assert p.isFull(3, 2);
+        assert perc == true;
+
+    }
+
+    private void printDebugGrid() {
+        for (int row = 0; row < gridSize; row++) {
+            for (int col = 0; col < gridSize; col++) {
+                int pos = row*gridSize + col;
+                System.out.print(openFull[pos] + " ");
+            }
+            System.out.println();
+        }
     }
 }
