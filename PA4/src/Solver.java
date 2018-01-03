8eb45f8580eea83abb25d7d40aa1ae26b4e048ae
maxxwizard <matthewhuynh88@gmail.com>
2018-01-03 15:24:42 -0500

    finished Solver class, tested against solvable and unsolvable puzzles

--- PA4/src/Solver.java
+++ PA4/src/Solver.java
@@ -10,41 +10,57 @@ public class Solver {
     Puzzle originalPuzzle;
     Puzzle twinPuzzle;
 
+    private static class Debugger {
+        public static void Log(String s) {
+            //StdOut.println(s); // remove the comment if we want debugging output
+        }
+    }
+
     private class Puzzle {
         private MinPQ<SearchNode> pq;
         private ArrayList<Board> gameTree;
         private SearchNode currentNode;
+        private boolean solved;
 
         public Puzzle(Board initial) {
             gameTree = new ArrayList<>();
             pq = new MinPQ<SearchNode>(new SearchNodeComparator());
             pq.insert(new SearchNode(initial, 0, null));
+        }
 
-            Iterable<Board> neighbors;
+        public boolean isSolved() {
+            return solved;
+        }
+
         /*
-        do {
-            remove search node from MinPQ
-            add search node to game tree
-            get neighbors of removed search node
-            prune neighbors to ensure it's not a board we've seen before (search our game tree)
-            add pruned neighbors to PQ
-        } while (!board.isGoal)
-         */
-            do {
-                currentNode = pq.delMin();
-                StdOut.println(String.format("removed from PQ (move #%d):\n%s", currentNode.moves, currentNode.board.toString()));
-                gameTree.add(currentNode.board);
-                neighbors = currentNode.board.neighbors();
-                pruneNeighbors(neighbors);
-                for (Board neighbor : neighbors) {
-                    pq.insert(new SearchNode(neighbor, currentNode.moves+1, currentNode));
-                    StdOut.println("added to PQ:\n" + neighbor.toString());
-                }
-                StdOut.println("----------------------------");
-            } while (!currentNode.board.isGoal());
+            Algorithm:
+                remove search node from MinPQ
+                add search node to game tree
+                get neighbors of removed search node
+                prune neighbors to ensure it's not a board we've seen before (search our game tree)
+                add pruned neighbors to PQ
 
-            // currentNode now contains a reference to the goal search node and we can walk it backwards to get solution
-            StdOut.println("goal:\n" + currentNode.board.toString());
+            Returns:
+                true if this puzzle instance has reached goal board, false otherwise
+         */
+        public void step() {
+            Iterable<Board> neighbors;
+            currentNode = pq.delMin();
+            Debugger.Log(String.format("removed from PQ (move #%d):\n%s", currentNode.moves, currentNode.board.toString()));
+            gameTree.add(currentNode.board);
+            neighbors = currentNode.board.neighbors();
+            pruneNeighbors(neighbors);
+            for (Board neighbor : neighbors) {
+                pq.insert(new SearchNode(neighbor, currentNode.moves+1, currentNode));
+                Debugger.Log("added to PQ:\n" + neighbor.toString());
+            }
+            Debugger.Log("----------------------------");
+
+            // currentNode will contains a reference to the goal search node
+            // and we can walk it backwards to get solution
+            if (currentNode.board.isGoal()) {
+                solved = true;
+            }
         }
 
         private void pruneNeighbors(Iterable<Board> neighbors) {
@@ -53,7 +69,7 @@ public class Solver {
                 Board b = it.next();
                 if (gameTree.contains(b)) {
                     // we've seen this board before so remove it
-                    StdOut.println("pruned:\n" + b.toString());
+                    Debugger.Log("pruned:\n" + b.toString());
                     it.remove();
                 }
             }
@@ -87,13 +103,20 @@ public class Solver {
     public Solver(Board initial) {
 
         originalPuzzle = new Puzzle(initial);
-        //boolean twin = solvePuzzle(initial.twin());
+        twinPuzzle = new Puzzle(initial.twin());
 
+        // we loop until one of the two puzzles has reached its goal board
+        do {
+            originalPuzzle.step();
+            twinPuzzle.step();
+        } while (!originalPuzzle.isSolved() && !twinPuzzle.isSolved());
+
+        // when we reach this point, it means either the original or the twin has been solved
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return originalPuzzle.currentNode != null;
+        return originalPuzzle.isSolved();
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
