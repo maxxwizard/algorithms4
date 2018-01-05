a9a9a9f0213670e6f054de43445f22461d8b592d
maxxwizard <matthewhuynh88@gmail.com>
2018-01-05 11:28:12 -0500

    implemented isEmpty() and size() in KdTree.java

--- PA5/src/KdTree.java
+++ PA5/src/KdTree.java
@@ -1,2 +1,86 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+import java.util.ArrayList;
+
 public class KdTree {
+
+    // even levels of the tree = vertical line
+    // odd levels of the tree = horizontal line
+
+    private Node root;
+
+    private static class Node {
+        private Point2D p;      // the point
+        private RectHV rect;    // the axis-aligned rectangle corresponding to this node
+        private Node lb;        // the left/bottom subtree
+        private Node rt;        // the right/top subtree
+
+        public Node(Point2D point) {
+            this.p = point;
+        }
+    }
+
+    // construct an empty set of points
+    public KdTree() {
+        this.root = null;
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return this.root.lb == null && this.root.rt == null;
+    }
+
+    // number of points in the set
+    public int size() {
+        return size(this.root);
+    }
+
+    private int size(Node n) {
+        if (n == null)
+            return 0;
+        else
+            return 1 + size(n.lb) + size(n.rt);
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
+
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
+
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null)
+            throw new java.lang.IllegalArgumentException();
+
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
+
+        if (this.size() == 0)
+            return null;
+
+        throw new java.lang.UnsupportedOperationException();
+    }
 }
--- PA5/src/KdTreeVisualizer.java
+++ PA5/src/KdTreeVisualizer.java
@@ -18,7 +18,7 @@ public class KdTreeVisualizer {
     public static void main(String[] args) {
         RectHV rect = new RectHV(0.0, 0.0, 1.0, 1.0);
         StdDraw.enableDoubleBuffering();
-        //KdTree kdtree = new KdTree();
+        KdTree kdtree = new KdTree();
         while (true) {
             if (StdDraw.isMousePressed()) {
                 double x = StdDraw.mouseX();
@@ -27,7 +27,7 @@ public class KdTreeVisualizer {
                 Point2D p = new Point2D(x, y);
                 if (rect.contains(p)) {
                     StdOut.printf("%8.6f %8.6f\n", x, y);
-                    //kdtree.insert(p);
+                    kdtree.insert(p);
                     StdDraw.clear();
                     //kdtree.draw();
                     StdDraw.show();
