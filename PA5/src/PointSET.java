5d06403d663335beca0ba64f52300ed8472014e8
maxxwizard <matthewhuynh88@gmail.com>
2018-01-05 10:59:38 -0500

    init PA5, finished PointSET.java

---
+++ PA5/src/PointSET.java
@@ -0,0 +1,90 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.ArrayList;
+
+public class PointSET {
+    private SET<Point2D> set;
+
+    // construct an empty set of points
+    public PointSET() {
+        this.set = new SET<Point2D>();
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return this.set.isEmpty();
+    }
+
+    // number of points in the set
+    public int size() {
+        return this.set.size();
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
+
+        this.set.add(p);
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
+
+        return this.set.contains(p);
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+        for (Point2D p : this.set) {
+            p.draw();
+        }
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null)
+            throw new java.lang.IllegalArgumentException();
+
+        ArrayList<Point2D> insidePoints = new ArrayList<Point2D>();
+
+        for (Point2D point : this.set) {
+            if (rect.contains(point)) {
+                insidePoints.add(point);
+            }
+        }
+
+        return insidePoints;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null)
+            throw new java.lang.IllegalArgumentException();
+
+        if (this.set.isEmpty())
+            return null;
+
+        Point2D nearest = null;
+        double shortestDistance = Double.POSITIVE_INFINITY;
+        for (Point2D point : this.set) {
+            double distance = p.distanceTo(point);
+            if (nearest == null || distance < shortestDistance) {
+                shortestDistance = distance;
+                nearest = point;
+            }
+        }
+
+        return nearest;
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+
+    }
+}
