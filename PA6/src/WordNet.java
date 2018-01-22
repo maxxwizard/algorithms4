82a8dc74e119cac7be7ac3b4079dfcfa73b6f439
maxxwizard <matthewhuynh88@gmail.com>
2018-01-21 19:39:57 -0500

    created constructor for synsets and hypernyms

--- PA6/src/WordNet.java
+++ PA6/src/WordNet.java
@@ -1,20 +1,53 @@
-import edu.princeton.cs.algs4.Bag;
-import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Digraph;
-import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.StdOut;
 
 public class WordNet {
 
-    // vertices and edges
-    Digraph graph;
+    // graph so we can calculate SAP and whatnot
+    private final Digraph graph;
 
-    // we'll store a dictionary of vertex-to-synonyms
-    Bag<String> synsets;
+    // we'll store a dictionary of synonym-to-integer (multivalued to single)
+    private final ST<String, Integer> map;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
 
-        throw new java.lang.UnsupportedOperationException();
+        if (synsets == null || hypernyms == null) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        // we need a symbol table to map strings to integers
+        map = new ST<>();
+
+        int countVertices = 0;
+        In in_synsets = new In(synsets);
+        while (in_synsets.hasNextLine()) {
+            String line = in_synsets.readLine();
+            countVertices++;
+            String[] tokens = line.split(",");
+            int synset = Integer.parseInt(tokens[0]);
+            StdOut.println(String.format("synset %d", synset));
+            for (String syn : tokens[1].split(" ")) {
+                StdOut.println(String.format(" syn %s", syn));
+                map.put(syn, synset);
+            }
+        }
+
+        graph = new Digraph(countVertices);
+        In in_hypernyms = new In(hypernyms);
+        while (in_hypernyms.hasNextLine()) {
+            String line = in_hypernyms.readLine();
+            String[] tokens = line.split(",");
+            int synset = Integer.parseInt(tokens[0]);
+            StdOut.println(String.format("synset %d", synset));
+            for (int i = 1; i < tokens.length; i++) {
+                int hypernym = Integer.parseInt(tokens[i]);
+                StdOut.println(String.format(" hypernym %d", hypernym));
+                graph.addEdge(synset, hypernym);
+            }
+        }
     }
 
     // returns all WordNet nouns
@@ -40,6 +73,6 @@ public class WordNet {
 
     // do unit testing of this class
     public static void main(String[] args) {
-
+        WordNet wordnet = new WordNet(args[0], args[1]);
     }
 }
